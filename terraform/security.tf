5ef6858207ba5f842cb8eed9d17961b2c73e20d2
Chieme20 <chiemezuomartina@gmail.com>
2026-06-23 19:52:26 +0100

    fix: clear final security group network declaration mapping

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,38 +1,25 @@
-
 resource "aws_security_group" "eks_cluster_sg" {
   name        = "project-bedrock-cluster-sg"
-  description = "Cluster control plane security group"
-  vpc_id      = aws_vpc.bedrock_vpc.id
+  description = "Cluster control plane communications"
+  vpc_id      = aws_default_vpc.bedrock_vpc.id
 
-  
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "project-bedrock-cluster-sg"
-  }
 }
 
 resource "aws_security_group" "eks_nodes_sg" {
   name        = "project-bedrock-node-sg"
-  description = "Security group for all nodes in the bedrock cluster"
-  vpc_id      = aws_vpc.bedrock_vpc.id
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = aws_default_vpc.bedrock_vpc.id
 
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-
-  ingress {
-    from_port       = 1025
-    to_port         = 65535
-    protocol        = "tcp"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     security_groups = [aws_security_group.eks_cluster_sg.id]
   }
 
@@ -41,9 +28,5 @@ resource "aws_security_group" "eks_nodes_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "project-bedrock-node-sg"
   }
 }
